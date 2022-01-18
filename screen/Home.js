c4ef7fe10efa691cbabf071f2c8f36f244909354
ihzarizkyk <ihzarizky30@gmail.com>
2022-01-18 21:50:13 +0700

    fix bug App.js and screen/Home.js

--- screen/Home.js
+++ screen/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, View, Text } from "react-native";
-import React from "react";
-import { Button, View, Text } from "react-native";
+
 export default class Home extends React.Component{
  
 render(){
